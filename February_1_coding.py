48c07e0150ebf3f70eb1cf3cfe9b1a33f3b7938b
Al-Arsenault201 <69997839+Al-Arsenault201@users.noreply.github.com>
2021-02-02 19:05:39 -0500

    Add files via upload

---
+++ February_1_coding.py
@@ -0,0 +1,50 @@
+# This is a comment
+# This will not be executed
+
+# A literal means use this exact value
+print ("Hello, World") # Hello, World is a literal
+
+# Python allows use of "  and ' equivalently
+# But you can't mix them
+
+# Type:
+# int:  an integer number
+# string: zero or more characters
+# float: real number 3.14159
+# bool: True or False not true or TRUE
+
+# Hello, World is a string
+# it's a string literal
+# all strings go between " " or ' '
+
+"""
+Anything in between here is treated as a comment.
+So if you have a whole lot of comments and you
+don't want to type all the #, put your comment
+in between these double quotes
+"""
+
+"""
+A variable is a symbol - it's the name of something
+that has a value and it can change
+It has a type
+"""
+
+a = "Hello, World"  #assignment statement
+print(a)
+
+a = "What a great class"
+print(a)
+
+# Constant - value that doesn't change
+pi = 3.14159
+
+# Python doesn't really support constants
+# In CMSC 201, we fake - use ALL CAPS
+PI = 3.14159
+
+#valid variable names
+circle_circumference
+circle2circumference
+CIRCLE
+2timesaround
